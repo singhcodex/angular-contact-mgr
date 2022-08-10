c1a8ef7f997dac02218cad3a39a74334a3935375
singhcodex <emosingh81@gmail.com>
2022-08-10 22:14:59 +0200

    add edit contact done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddContactComponent } from './components/add-contact/add-contact.component';
 import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
 import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ViewContactComponent } from './components/view-contact/view-contact.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'contacts/add', component: AddContactComponent},
   {path: 'contacts/edit/:contactId', component: EditContactComponent},
   {path: 'contacts/view/:contactId', component: ViewContactComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
